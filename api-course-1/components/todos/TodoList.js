04baa2b8d2e66f7fc4d276f999a5fa79efb53ccc
pouriaH9 <pouriah9@gmail.com>
2022-08-09 20:51:20 +0430

    todo style changed

--- api-course-1/components/todos/TodoList.js
+++ api-course-1/components/todos/TodoList.js
@@ -9,20 +9,20 @@ const TodoList = ({ data, onDelete }) => {
             return (
               <div
                 key={todo.id}
-                className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl shadow "
+                className="transiotion-all duration-300 hover:border-indigo-500 hover:p-6 text-white text-xl flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl shadow "
               >
                 <h1 className="font-medium">{todo.title}</h1>
-                <div className="flex bg-white p-1 ">
-                  <button className="flex items-center justify-between mr-3">
+                <div className="flex p-1 ">
+                  <button className="cursor-pointer flex items-center justify-between mr-3">
                     <CheckIcon className="h-7 w-7 text-green-500" />
                   </button>
                   <button
                     onClick={() => onDelete(todo.id)}
-                    className="flex items-center justify-between mr-3 "
+                    className="cursor-pointer flex items-center justify-between mr-3 "
                   >
                     <TrashIcon className="h-5 w-5 text-red-500" />
                   </button>
-                  <button className="flex items-center justify-between mr-3">
+                  <button className="cursor-pointer flex items-center justify-between mr-3">
                     <PencilAltIcon className="h-5 w-5 text-blue-500" />
                   </button>
                 </div>
