ccf3018c7f4ab2de288f200ac31216a0f132d08a
pouriaH9 <pouriah9@gmail.com>
2022-08-09 17:32:20 +0430

    todo v 0.1

---
+++ api-course-1/components/todos/AddNewTodo.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+
+const TodoForm = ({ onAdd }) => {
+  const [formData, setFormData] = useState({ title: "", description: "" });
+  const [isShow, setIsShow] = useState(false);
+
+  const changeHandeler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  console.log(formData);
+  if (!isShow) {
+    return (
+      <div className="text-center mt-10 ">
+        <button
+          onClick={() => setIsShow(true)}
+          className="bg-transparent hover:bg-white text-white font-semibold hover:text-stone-900 py-2 px-4 border border-white hover:border-transparent rounded"
+        >
+          Add new todo
+        </button>
+      </div>
+    );
+  }
+  return (
+    <div className="mr-auto ml-auto mt-10 w-1/2  flex justify-center border rounded">
+      <form
+        className="px-8 pt-6 pb-8 mb-4"
+        onSubmit={(e) => onAdd(e, formData)}
+      >
+        <div className="">
+          <label
+            className="justify-start text-xl text-white mb-1 block "
+            htmlFor="todo-title"
+          >
+            Title
+          </label>
+          <input
+            name="title"
+            placeholder="write a todo..."
+            id="todo-title"
+            className=" w-max bg-stone-900 border rounded border-white p-3 focus:outline-none focus:border-indigo-500	text-white"
+            type="text"
+            value={formData.title}
+            onChange={changeHandeler}
+          />
+        </div>
+        <div className="">
+          <label
+            className="justify-start text-xl text-white mb-1 block mt-4"
+            htmlFor="todo-description"
+          >
+            Description
+          </label>
+          <textarea
+            value={formData.description}
+            name="description"
+            onChange={changeHandeler}
+            id="todo-description"
+            className="bg-stone-900 border rounded border-white p-3 focus:outline-none focus:border-indigo-500	text-white"
+          ></textarea>
+        </div>
+        <div className="mt-2">
+          <button
+            onClick={() => setIsShow(false)}
+            type="button"
+            className="bg-transparent hover:bg-white text-white font-semibold hover:text-stone-900 py-2 px-4 border border-white hover:border-transparent rounded mr-3"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="bg-transparent hover:bg-white text-white font-semibold hover:text-stone-900 py-2 px-4 border border-white hover:border-transparent rounded"
+          >
+            Add new todo
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
---
+++ api-course-1/components/todos/TodoList.js
@@ -0,0 +1,38 @@
+import { CheckIcon, PencilAltIcon, TrashIcon } from "@heroicons/react/solid";
+
+const TodoList = ({ data, onDelete }) => {
+  return (
+    <div className="container p-2 xl:max-w-screen-xl mx-auto">
+      <section className="flex items-center justify-center">
+        <div className="w-full max-w-screen-md p-2 md:p-4 rounded-xl ">
+          {data.map((todo) => {
+            return (
+              <div
+                key={todo.id}
+                className="flex items-center justify-between border border-gray-100 mb-4 p-3 md:p-4 rounded-xl shadow "
+              >
+                <h1 className="font-medium">{todo.title}</h1>
+                <div className="flex bg-white p-1 ">
+                  <button className="flex items-center justify-between mr-3">
+                    <CheckIcon className="h-7 w-7 text-green-500" />
+                  </button>
+                  <button
+                    onClick={() => onDelete(todo.id)}
+                    className="flex items-center justify-between mr-3 "
+                  >
+                    <TrashIcon className="h-5 w-5 text-red-500" />
+                  </button>
+                  <button className="flex items-center justify-between mr-3">
+                    <PencilAltIcon className="h-5 w-5 text-blue-500" />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default TodoList;
